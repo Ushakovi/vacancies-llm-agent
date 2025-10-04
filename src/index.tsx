3300e4674fec1ce4dadae650e80ee5eb4b6d43cb
Ivan Ushakov <van578@yandex.ru>
2025-10-04 12:39:16 +0300

    test44

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './app/app';
 
-const rootElement123 = document.getElementById('root');
+const rootElement1233 = document.getElementById('root');
 
-if (!rootElement123) {
+if (!rootElement1233) {
     throw new Error('Element with id app not found in the DOM');
 }
 
-const rootNode = ReactDOM.createRoot(rootElement123);
+const rootNode = ReactDOM.createRoot(rootElement1233);
 rootNode.render(<App />);
